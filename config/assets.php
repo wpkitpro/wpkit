26f8e3e3aa957927114ce9a6c41fd276a9b3ee66
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-10 14:06:49 +0100

    add function description

--- config/assets.php
+++ config/assets.php
@@ -1,6 +1,16 @@
 <?php
  namespace WpKit;
 
+ /**
+  * Retrieves the URL of assets in the theme.
+  *
+  * Searches in the stylesheet directory before the template directory so themes
+  * which inherit from a parent theme can just override one file.
+  *
+  * @param $file
+  *
+  * @return string
+  */
 function assets( $file ) {
     return get_theme_file_uri("/public/$file");
 }
