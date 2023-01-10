b3ef99c72a73c1d05a608e399054feecd20ca431
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-10 21:11:05 +0100

    initial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,22 +25,6 @@ if ( ! function_exists( 'collect' ) ) {
     }
 }
 
-
-/**
- * --------------------------------------------------------------------------
- * Register The Auto Loader
- * --------------------------------------------------------------------------
- * Composer provides a convenient, automatically generated class loader for
- * our theme. We will simply require it into the script here so that we
- * don't have to worry about manually loading any of our classes later on.
- *
- */
-// if(! file_exists($composer = __DIR__ . '/vendor/autoload.php') ) {
-//     wp_die(__('Error locating autoloader. Please run <code>composer install</code>.', 'WpKit'));
-// }
-//
-// require $composer;
-
 /**
  * --------------------------------------------------------------------------
  * Register WpKit Theme Configuration
