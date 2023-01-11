8ff73e00cc8f5767b6a147ce3ba0d8537c237906
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-11 17:15:04 +0100

    Add index page content

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
     <?php wp_body_open(); ?>
     <?php do_action('get_header') ?>
 
-    <div id="app"></div>
+    <div id="app">
+      <?php if (have_posts()){
+        while (have_posts()) { the_post();
+          get_the_content();
+        }
+      } ?>
+    </div>
 
     <?php do_action('get_footer') ?>
     <?php wp_footer(); ?>
