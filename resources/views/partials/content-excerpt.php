17e153a257e01d6414e461e0219e1310f9d8c214
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-15 12:53:37 +0100

    Remove .container

--- resources/views/partials/content-excerpt.php
+++ resources/views/partials/content-excerpt.php
@@ -1,5 +1,3 @@
-<div class="container">
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php wk_template( 'partials/excerpt', get_post_format() ); ?>
-  </article>
-</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php wk_template( 'partials/excerpt', get_post_format() ); ?>
+</article>
