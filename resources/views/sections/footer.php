58db6c66507bacd369026a1bdba219fda6a39c6b
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-16 05:44:01 +0100

    Add .container to footer tag

--- resources/views/sections/footer.php
+++ resources/views/sections/footer.php
@@ -1,5 +1,4 @@
-<footer id="colophon">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer') ?>
-  </div>
+<footer id="colophon" class="container">
+  <div class="border-top border-1 my-3"></div>
+  <?php dynamic_sidebar('sidebar-footer') ?>
 </footer>
