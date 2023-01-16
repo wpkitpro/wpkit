775bc1ca9e512e2885c3e27e14704397cc500f86
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-16 16:26:25 +0100

    Fixed global template single.php

--- single.php
+++ single.php
@@ -1,10 +1,12 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all single posts
+ */
 
-<?php if ( have_posts() ): ?>
-  <?php while ( have_posts() ) {
-    the_post();
-    wk_template( 'partials/content', 'single' );
-  } ?>
-<?php endif; ?>
+get_header();
 
-<?php get_footer(); ?>
+while ( have_posts() ) { the_post();
+  wk_template( 'partials/content', 'single' );
+}
+
+get_footer();
