c0ba8b198dbc220299640305dd823288fa57df21
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-21 11:22:09 +0100

    Update Template Loader

--- includes/template-loader.php
+++ includes/template-loader.php
@@ -3,49 +3,46 @@
  * Loads the correct template based on the visitor's url
  */
 
-if ( wp_using_themes() ) {
-	// Template types
-	$types = [
-		'404',
-		'archive',
-		'attachment',
-		'author',
-		'category',
-		'date',
-		'embed',
-		'frontpage',
-		'home',
-		// 'index',
-		'page',
-		'paged',
-		'privacypolicy',
-		'search',
-		'single',
-		'singular',
-		'tag',
-		'taxonomy',
-	];
+// Template types
+$types = [
+	'404',
+	'archive',
+	'attachment',
+	'author',
+	'category',
+	'date',
+	'embed',
+	'frontpage',
+	'home',
+	// 'index',
+	'page',
+	'paged',
+	'privacypolicy',
+	'search',
+	'single',
+	'singular',
+	'tag',
+	'taxonomy',
+];
+
+// Add Filter
+foreach ( $types as $type ) {
+	add_filter( "{$type}_template_hierarchy", "prefix_template_hierarchy", 20 );
+}
+
+function prefix_template_hierarchy( $templates ) {
+	$type = str_replace( '_template_hierarchy', '', current_filter() );
 	
-	// Add Filter
-	foreach ( $types as $type ) {
-		add_filter( "{$type}_template_hierarchy", "prefix_template_hierarchy", 20 );
+	// add `taxonomy.php` file after last `category.php` and `tag.php` element.
+	if ( in_array( $type, [ 'category', 'tag' ] ) ) {
+		$templates[] = 'taxonomy.php';
 	}
 	
-	function prefix_template_hierarchy( $templates ) {
-		$template_dir = 'resources/views';
-		$type         = str_replace( '_template_hierarchy', '', current_filter() );
-		
-		// add `taxonomy.php` file after last `category.php` and `tag.php` element.
-		if ( in_array( $type, [ 'category', 'tag' ] ) ) {
-			$templates[] = 'taxonomy.php';
+	foreach ( $templates as $key => $template ) {
+		if ( ! str_starts_with( $template, VIEWS_DIR ) ) {
+			$templates[ $key ] = VIEWS_DIR . '/' . $template;
 		}
-		
-		foreach ( $templates as $key => $template ) {
-			if ( ! str_starts_with( $template, $template_dir ) ) {
-				$templates[ $key ] = "{$template_dir}/$template";
-			}
-		}
-		
-		return $templates;
 	}
+	
+	return $templates;
 }
