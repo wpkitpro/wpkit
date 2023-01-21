3eb0d27c91dab1fb4dd5d8bbba564372b94a90a3
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-21 07:17:07 +0100

    Create views templates hierarchy

---
+++ resources/views/404.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+404 Page Template from VIEWS
+<?php get_footer(); ?>
---
+++ resources/views/page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying all single posts
+ */
+
+get_header();
+echo 'page template';
+
+get_footer();
