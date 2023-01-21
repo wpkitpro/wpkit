1545b0170b12e8cdb405067ff568c8f2402f9a30
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-21 11:28:52 +0100

    Update template hierarchy

--- includes/template-loader.php
+++ includes/template-loader.php
@@ -14,7 +14,6 @@ $types = [
 	'embed',
 	'frontpage',
 	'home',
-	// 'index',
 	'page',
 	'paged',
 	'privacypolicy',
@@ -27,22 +26,20 @@ $types = [
 
 // Add Filter
 foreach ( $types as $type ) {
-	add_filter( "{$type}_template_hierarchy", "prefix_template_hierarchy", 20 );
-}
-
-function prefix_template_hierarchy( $templates ) {
-	$type = str_replace( '_template_hierarchy', '', current_filter() );
-	
-	// add `taxonomy.php` file after last `category.php` and `tag.php` element.
-	if ( in_array( $type, [ 'category', 'tag' ] ) ) {
-		$templates[] = 'taxonomy.php';
-	}
-	
-	foreach ( $templates as $key => $template ) {
-		if ( ! str_starts_with( $template, VIEWS_DIR ) ) {
-			$templates[ $key ] = VIEWS_DIR . '/' . $template;
+	add_filter( "{$type}_template_hierarchy", function ( $templates ) {
+		$type = str_replace( '_template_hierarchy', '', current_filter() );
+		
+		// add `taxonomy.php` file after last `category.php` and `tag.php` element.
+		if ( in_array( $type, [ 'category', 'tag' ] ) ) {
+			$templates[] = 'taxonomy.php';
 		}
-	}
-	
-	return $templates;
+		
+		foreach ( $templates as $key => $template ) {
+			if ( ! str_starts_with( $template, VIEWS_DIR ) ) {
+				$templates[ $key ] = VIEWS_DIR . '/' . $template;
+			}
+		}
+		
+		return $templates;
+	}, 20 );
 }
