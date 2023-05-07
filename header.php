ec4647e87fb7d366ecee0bf56229bbf36251099d
garikhg <garik.28h@gmail.com>
2023-05-08 03:21:07 +0400

    Renamed function from wk_the_html_classes() to wpkit_html_classes()

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?> <?php wk_the_html_classes(); ?>>
+<html <?php language_attributes(); ?> <?php wpkit_html_classes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
