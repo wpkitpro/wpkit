4a7ebd69d78d14ca8d16df9de1d5895c8ec5b9f8
garikhg <garik.28h@gmail.com>
2023-05-08 00:57:44 +0400

    Moved page from resources/views and change wk_template to get_template_part

--- page.php
+++ page.php
@@ -6,8 +6,8 @@
 get_header();
 
 while ( have_posts() ) { the_post();
-  wk_template( 'partials/page', 'header' );
-  wk_template( 'partials/content', 'page' );
+  get_template_part( 'parts/header/page-header' );
+  get_template_part( 'parts/content/content-page' );
 }
 
 get_footer();
