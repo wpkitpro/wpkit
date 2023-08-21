ecb547b750238430bde51d8e572b3ee11278e13a
garikhg <garik.28h@gmail.com>
2023-08-21 23:34:17 +0400

    Deleted demo content from site-header for a more comfortable start

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -7,34 +7,21 @@ $blog_name = get_bloginfo( 'name' );
 ?>
 <header id="masthead" class="banner">
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
+    <div class="container-fluid">
       <?php if ( $blog_name ): ?>
         <a href="<?= esc_url( home_url( '/' ) ) ?>" class="navbar-brand">
         <?php echo esc_html( $blog_name ); ?>
         </a>
       <?php endif; ?>
 
-      <?php if ( has_nav_menu( 'primary' ) ): ?>
-        <button class="navbar-toggler"  type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#primaryNavigation"
-                aria-controls="primaryNavigation"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="primaryNavigation">
-          <?php wp_nav_menu( [
-            'theme_location' => 'primary',
-            'menu_class'     => 'navbar-nav me-auto mb-2 mb-lg-0 gap-3',
-            'container'      => false,
-            'items_wrap'     => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-            'fallback_cb'    => false,
-          ] ); 
-          ?>
-        </div>
-      <?php endif; ?>
-    </div>
+      <?php wp_nav_menu( [
+        'theme_location'  => 'primary',
+        'menu_class'      => 'navbar-nav me-auto mb-2 mb-lg-0 gap-3',
+        'container_class' => 'primary-navigation-container',
+        'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+        'fallback_cb'     => false,
+      ] ); 
+      ?>
+    </div><!-- .container-fluid -->
   </nav>
 </header>
