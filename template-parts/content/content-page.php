9809041bf1b232667671940957dc98f29dc83cad
garikhg <garik.28h@gmail.com>
2023-08-22 00:03:21 +0400

    Changed content-page.php

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,10 +1,3 @@
 <article id="<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php
-    the_content();
-
-    wp_link_pages( [
-        'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'wpkit' ),
-        'after'  => '</p></nav>',
-    ] );
-    ?>
+    <?php the_content(); ?>
 </article>
