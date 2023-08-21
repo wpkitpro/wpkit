b2ab2106ff6d37709ac9c28b33614c31c3b0a330
garikhg <garik.28h@gmail.com>
2023-08-22 00:02:38 +0400

    Added teplate part file content.php

---
+++ template-parts/content/content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php the_content(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
