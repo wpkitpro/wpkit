8c294346e832adffcba7ba4069cd5c43f9552a07
garikhg <garik.28h@gmail.com>
2023-08-21 23:28:16 +0400

    Added description about teplate file header.php

--- header.php
+++ header.php
@@ -1,4 +1,13 @@
-<!doctype html>
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ */
+
+?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>"/>
