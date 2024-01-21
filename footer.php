b66f2db70dbe53b4dbe881376a3419c4981691ec
garikhg <garik.28h@gmail.com>
2024-01-21 20:25:20 +0400

    Adjust footer structure in wpkit theme
    The structure of the footer.php file in the wpkit theme has been modified. This includes altering the wrapped element for site content and reformatting the wp_footer() invocation. These changes ensure appropriate content encapsulation and improve code readability.

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-</main><!-- #main -->
+</div><!-- #content -->
 
 <footer id="colophon" class="container">
 	<?php dynamic_sidebar( 'sidebar-footer' ) ?>
 </footer>
-</div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php
+wp_footer(); ?>
 
 </body>
 </html>
