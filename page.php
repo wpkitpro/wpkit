c61f481af53903e050e1728ef219d9b3046c01b4
garikhg <garik.28h@gmail.com>
2024-01-21 20:20:07 +0400

    Refactor page.php in WPkit theme
    Revised the structure of page.php file in the WPKit theme to allow for more features. Changes include adding post thumbnail display, entry title rendering, content display, pagination, and comments handling. This will make the posts on the site more engaging and interactive.

--- page.php
+++ page.php
@@ -5,10 +5,46 @@
 
 get_header();
 
-while ( have_posts() ) {
-	the_post();
-	get_template_part( 'template-parts/header/page-header' );
-	get_template_part( 'template-parts/content/content-page' );
-}
+?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<article id="page-<?php echo esc_attr( the_ID() ) ?>" <?php post_class(); ?>>
+				<?php
+				// Loop Start
+				while ( have_posts() ): the_post();
+					if ( has_post_thumbnail() ) {
+						echo '<div class="post-media">';
+						the_post_thumbnail();
+						echo '</div>';
+					}
 
+					if ( get_the_title() !== '' ) {
+						the_title( '<h1 class="page-title">', '</h1>' );
+					}
+
+					echo '<div class="post-content">';
+					the_content();
+					echo '</div>';
+
+					// Post Pagination
+					$args = array(
+						'before' => '<p class="single-pagination">' . esc_html__( 'Pages:', 'wpkit' ),
+						'after'  => '</p>'
+					);
+					wp_link_pages( $args );
+				endwhile; // Loop End
+				?>
+			</article><!-- #page-<?php echo esc_attr( the_ID() ) ?>-->
+
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				echo '<div id="comments" class="comments-area">';
+				comments_template( '', true );
+				echo '</div><!-- #comments -->';
+			}
+			?>
+		</main>
+	</div>
+<?php
 get_footer();
